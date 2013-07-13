5e3f52996284a324f424116edd6b68fed6ef544e
Dimitar Roustchev <d.roustchev@googlemail.com>
2013-07-13 17:30:34 +0200

    * Adds catch_all route * Adds pymongo requirement * Adds init config

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 from flask import Flask, render_template
+from flask import jsonify
+from pymongo import MongoClient
 
+import ConfigParser
 
 app = Flask(__name__)
 app.debug = True
 
+#read config file
+config = ConfigParser.ConfigParser()
+config.read('foodle.ini')
+mongodb_url = config.get('MongoDB', 'url')
+
+#db init
+#mongo = MongoClient(mongodb_url)
+
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
