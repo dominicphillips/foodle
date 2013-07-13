0e473e932efcdac338ead334e228ab3ef4d0d443
Dimitar Roustchev <d.roustchev@googlemail.com>
2013-07-13 22:45:17 +0200

    Added test data.

---
+++ mongogimport/dishes.js
@@ -0,0 +1,2 @@
+{ "_id" : 1, "name" : "Pizza Tonno", "price" : 7.5, "health_score" : 2, "user_rating" : 4, "nutrition" : [], "food_class" : [], "restaurant" : 1 }
+{ "_id" : 2, "name" : "Pizza Vegetariana", "price" : 6, "health_score" : 4, "user_rating" : 3, "nutrition" : [], "food_class" : [ "vegitarian" ], "restaurant" : 1 }
