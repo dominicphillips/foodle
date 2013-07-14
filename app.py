785dfb1689622ba77d94d3563e3c5e710b085162
Dimitar Roustchev <d.roustchev@googlemail.com>
2013-07-14 14:01:33 +0200

    Added user handling via Flask Login.

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient, GEOSPHERE
 from bson.son import SON
 from werkzeug.contrib.fixers import ProxyFix
-import math
+from werkzeug.security import generate_password_hash, \
+    check_password_hash
 
+import math
+import re
 import ConfigParser
 
 app = Flask(__name__)
@@ -20,12 +23,92 @@ db = mongo.foodle
 col_dishes = db.dishes
 col_restaurants = db.restaurants
 col_restaurants.ensure_index([("location", GEOSPHERE)])
+col_users = db.users
+
+login_manager = LoginManager()
+
+
+class User(object):
+    def __init__(self, name, email, passwd):
+        self.name = name
+        self.email = email
+        self.set_passwd = passwd
+
+    def set_passwd(self, passwd):
+        self.pw_hash = generate_password_hash(passwd)
+
+    def check_passwd(self, passwd):
+        return check_password_hash(self.pw_hash, passwd)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.email
+
+
+@login_manager.user_loader
+def load_user(email):
+    existing_user = col_users.find_one({'email': email})
+    if not existing_user:
+        return None
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/api/users/register/')
+def register_user():
+    if request.form and request.form.name and request.form.email and request.form.password:
+        form_data = request.form
+        form_name = form_data.name[0]
+        form_email = form_data.email[0]
+        form_pwd = form_data.password[0]
+        existing_user = col_users.find_one({'email': form_email})
+        if existing_user:
+            return jsonify({
+                'status': 'error',
+                'msg': "This e-mail is already registered!"
+            })
+        else:
+            return jsonify({
+                'status': 'error',
+                'msg': "Please fill out all fields!"
+            })
+    else:
+        user = {
+            'name': form_name,
+            'email': form_email,
+            'pwd': generate_password_hash(form_pwd)
+        }
+        col_users.insert(user)
+        return jsonify({'status': 'success'})
+
+
+@app.rout('/api/users/login/')
+def login_user():
+    if reques.form and requst.form.login and request.form.password:
+        login = request.form.login
+        password = request.form.password
+        if not re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", login):
+            user = col_users.fetch_one({'name': login})
+            if user and (user.pwd == check_password_hash(user.pwd, password)):
+
+        else:
+
+    else:
+        return jsonify({
+            'status': 'error',
+            'msg': "Sorry, something went wrong!"
+        })
 
 @app.route('/api/dishes/')
 def retrieve_dishes():
